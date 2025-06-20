b6ec9700c0123519d8ba29bb78455e51de0a9fde
reterrr <sharonovnikola1@gmail.com>
2025-06-20 19:44:37 +0200

    fingerprint added

--- app/src/main/java/com/example/p2p/Request/Sender.java
+++ app/src/main/java/com/example/p2p/Request/Sender.java
@@ -5,7 +5,11 @@ import android.os.Build;
 import com.example.p2p.InetAddressDeserializer;
 
 import com.example.p2p.Model.User;
+import com.example.p2p.Model.UserKeys;
+import com.example.p2p.Model.UserKeys_;
+import com.example.p2p.Model.User_;
 import com.example.p2p.NetworkResourceManager;
+import com.example.p2p.ObjectBox;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -13,18 +17,19 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import io.objectbox.Box;
+import io.objectbox.query.Query;
+import io.objectbox.query.QueryBuilder;
 
-@Getter
-@Setter
 public class Sender {
     @JsonProperty("username")
     public String username;
     @JsonProperty("address")
     @JsonDeserialize(using = InetAddressDeserializer.class)
     public InetAddress address;
+
+    @JsonProperty("finger_print")
+    public String fingerPrint;
 
     @JsonProperty("port")
     public long port;
@@ -35,15 +40,25 @@ public class Sender {
             @JsonProperty("address")
             @JsonDeserialize(using = InetAddressDeserializer.class)
             InetAddress address,
-            @JsonProperty("port") long port
+            @JsonProperty("port") long port,
+            @JsonProperty("finger_print") String fingerPrint
     ) {
         this.address = address;
         this.port = port;
         this.username = username;
+        this.fingerPrint = fingerPrint;
     }
 
     public static Sender fromUser(User user) throws UnknownHostException {
-        return new Sender(username2post(user.username), InetAddress.getByName(user.networkInfo.getTarget().ip), user.networkInfo.getTarget().port);
+        Box<UserKeys> userKeysBox = ObjectBox.get().boxFor(UserKeys.class);
+        QueryBuilder<UserKeys> q = userKeysBox.query();
+        q.link(UserKeys_.user)
+                .equal(User_.id, user.id);
+
+        return new Sender(username2post(user.username),
+                InetAddress.getByName(user.networkInfo.getTarget().ip),
+                user.networkInfo.getTarget().port,
+                q.build().findFirst().fingerPrint);
     }
 
     public static String username2post(String username) {
