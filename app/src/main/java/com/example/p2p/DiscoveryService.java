f55b90aa9d9ad4fa4a7cf756a02a1fc3664665cf
reterrr <sharonovnikola1@gmail.com>
2025-06-20 19:50:01 +0200

    localhost user chat

--- app/src/main/java/com/example/p2p/DiscoveryService.java
+++ app/src/main/java/com/example/p2p/DiscoveryService.java
@@ -80,6 +80,12 @@ public class DiscoveryService extends Service {
         String ip = info.getInetAddresses()[0].getHostAddress();
         long port = info.getPort();
 
+        User cuser = CurrentUserManager.getUser().user.getTarget();
+        NetworkInfo ninfo = cuser.networkInfo.getTarget();
+
+        if (nickname.equals(Sender.username2post(cuser.username)) && ip.equals(ninfo.ip) && port == ninfo.port)
+            return;
+
         try {
             Peer peer = new Peer(nickname, InetAddress.getByName(ip), (int) port);
             PeerRepository.getInstance().addPeer(peer);
